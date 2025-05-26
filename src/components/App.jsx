e5ea1850d8a858ef9e8cfad9e429c2e469383879
nata20019 <nbernikova68@gmail.com>
2025-05-26 21:14:31 +0200

    mod3987654456

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,84 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import Section from './Section/Section';
+import ContactForm from './ContactForm/ContactForm';
+import ContactsSearch from './ContactsSearch';
+import ContactsList from './ContactsList';
+import contactsJson from '../assets/contacts.json';
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: contactsJson,
+    filter: '',
+  };
+  handleAddContact = newContact => {
+    if (
+      this.state.contacts.some(
+        contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+      )
+    ) {
+      alert(`${newContact.name} is already in contacts.`);
+      return;
+    }
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { id: nanoid(), ...newContact }],
+    }));
+  };
+
+  handleDelete = id => {
+    this.setState(prevState => {
+      const newContactsList = prevState.contacts.filter(
+        contact => contact.id !== id
+      );
+      return { contacts: newContactsList };
+    });
+  };
+
+  handleFilterChange = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  render() {
+    // const { contacts } = this.state;
+    const { filter } = this.state;
+    const filteredContacts = this.getFilteredContacts();
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'block',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 20,
+          color: '#010101',
+          margin: 100,
+        }}
+      >
+        React homework template
+        <Section title="Phonebook">
+          <ContactForm onAddContact={this.handleAddContact} />
+        </Section>
+        <Section title="Contacts">
+          <ContactsSearch
+            filter={filter}
+            onFilterChange={this.handleFilterChange}
+          />
+          <ContactsList
+            contacts={filteredContacts}
+            onDelete={this.handleDelete}
+          />
+        </Section>
+      </div>
+    );
+  }
+}
